b77b15cde4ee0bfbfd346ac89ad68122b2d6cd88
s29a <r.v.pisarev@gmail.com>
2026-01-31 15:41:22 +0500

    add sensors

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <filesystem>
 #include <nvml.h>
+#include "sensors.hpp"
 
 namespace fs = std::filesystem;
 
@@ -20,6 +21,16 @@ int main() {
     nvmlShutdown();
   } else {
     std::cerr << "[WARN] NVML failed:" << nvmlErrorString(result) << std::endl;
+  }
+
+  SysfsSensor cpu_temp("CPU", "/sys/class/hwmon/hwmon1/temp1_input");
+
+  std::cout << "Starting sensor monitor..." << std::endl;
+
+  for(int i = 0; i < 5; ++i) {
+    double t = cpu_temp.read_temp();
+    std::cout << "Current " << cpu_temp.get_name() << " temp:" << t << "°C" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
   }
 
   return 0;
